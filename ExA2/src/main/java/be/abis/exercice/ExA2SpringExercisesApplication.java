14262457395140072e6176057d696756a770a029
Philippe Peeters <philippe.a.peeters@bnpparibasfortis.com>
2020-10-06 13:29:59 +0200

    ExA2 AbisCourseService implementation

---
+++ ExA2/src/main/java/be/abis/exercice/ExA2SpringExercisesApplication.java
@@ -0,0 +1,13 @@
+package be.abis.exercice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExA2SpringExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExA2SpringExercisesApplication.class, args);
+	}
+
+}
