7197362e1eb1776e3da1f0e67251fd2fa90d0932
Philippe Peeters <philippe.a.peeters@bnpparibasfortis.com>
2020-10-06 10:58:33 +0200

    HelloSpring

---
+++ HelloSpring/src/main/java/be/abis/hello/model/Person.java
@@ -0,0 +1,25 @@
+package be.abis.hello.model;
+
+public class Person {
+
+	private String firstName;
+	
+	public Person() {
+		super();
+	}
+
+	public Person(String firstName) {
+		super();
+		this.firstName = firstName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	
+}
---
+++ HelloSpring/src/main/java/be/abis/hello/service/HelloService.java
@@ -0,0 +1,8 @@
+package be.abis.hello.service;
+
+import be.abis.hello.model.Person;
+
+public interface HelloService {
+
+	Person findPerson(int id);
+}
