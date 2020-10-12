e26995fc18b1056b73af13a6538d823725ac0e5f
Philippe Peeters <philippe.a.peeters@bnpparibasfortis.com>
2020-10-12 09:51:50 +0200

    ExC1 - Validations

---
+++ ExC1/src/main/java/be/abis/exercice/ExC1SpringExercisesApplication.java
@@ -0,0 +1,13 @@
+package be.abis.exercice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExC1SpringExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExC1SpringExercisesApplication.class, args);
+	}
+
+}
